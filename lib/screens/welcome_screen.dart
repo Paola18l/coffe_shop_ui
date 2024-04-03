26b592defd68621fcbb43f3dab199ba1b8eaf293
Cristipaola18@gmail.com <Cristipaola18@gmail.com>
2024-04-02 21:12:52 -0600

    trabajamos en el welcome_screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import "package:google_fonts/google_fonts.dart";
 
 class welcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var GoogleFonts;
     return Material(
       child: Container(
-        padding: EdgeInsets.only(top: 100, bottom: 40),
+        padding: EdgeInsets.only(
+            top: 100,
+            bottom:
+                40), // se utiliza para definir márgenes o rellenos en un widget
         decoration: BoxDecoration(
           color: Colors.black,
           image: DecorationImage(
             image: AssetImage("images/bug.png"),
+            fit: BoxFit
+                .cover, // la imagen se ajustará dentro del widget para cubrirlo completamente
+            opacity: 0.6,
           ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              //vamos al google fonts y copiamos y lo colocamos debajo de cupertino
+              "coffee Shop",
+              style: GoogleFonts.pacifico(
+                fontSize: 50,
+                Colors.white,
+              ),
+            ),
+          ],
         ),
       ),
     );
